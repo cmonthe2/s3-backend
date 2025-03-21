fc7112b97eee4101252f5681ee5d278fad82dda0
cameron <cmonthe8@outlook.com>
2025-03-21 11:53:37 -0400

    code update

--- state-setup.tf
+++ state-setup.tf
@@ -1,15 +1,35 @@
 # This file is used to create the S3 bucket and DynamoDB table that will be used to store the Terraform state file and lock information.
 
+# S3 Bucket for Terraform State
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
-  lifecycle {
-    prevent_destroy = true
-  }
-  versioning {
-    enabled = true
-  }
+  force_destroy = false  # Prevent accidental deletion
   tags = {
     Name = "Terraform State Bucket"
+  }
+}
+# Set the bucket ACL to private
+resource "aws_s3_bucket_acl" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  acl = "private"
+}
+# Enable versioning separately
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable encryption for security
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
 
